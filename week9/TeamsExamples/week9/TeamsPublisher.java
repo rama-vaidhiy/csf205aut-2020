b6f824a697c94d874857c514a44258e0a4f05100
rama-swuni <r.vaidhiyanathan@swansea.ac.uk>
2020-10-09 11:17:16 +0100

    Examples for Labs

---
+++ week9/TeamsExamples/week9/TeamsPublisher.java
@@ -0,0 +1,12 @@
+package week9;
+
+import javax.xml.ws.Endpoint;
+
+class TeamsPublisher {
+	public static void main(String[] args) {
+		int port = 8888;
+		String url = "http://localhost:" + port + "/teams";
+		System.out.println("Publishing Teams on port " + port);
+		Endpoint.publish(url, new Teams());
+	}
+}
